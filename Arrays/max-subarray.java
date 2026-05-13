1994566e837bc9d5de0796337b431d0de7468461
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-13 11:11:00 +0530

    Update max-subarray.java

--- Arrays/max-subarray.java
+++ Arrays/max-subarray.java
@@ -1,1 +1,36 @@
+import java.util.*;
 
+public class Main {
+
+    // Function to find maximum subarray sum
+    public static int maxSubArray(int[] arr) {
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Output maximum subarray sum
+        System.out.println(maxSubArray(arr));
+
+        sc.close();
+    }
+}
